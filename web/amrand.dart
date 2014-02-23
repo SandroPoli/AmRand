7cccdcec820b7dec1443e06acaab1e40c6200c8a
innubili <rudy@innubili.com>
2014-02-23 22:00:06 +0100

    link in editor

--- web/amrand.dart
+++ web/amrand.dart
@@ -30,7 +30,6 @@ void main() {
   qteBox = querySelector('#quoteBox');
   editor = querySelector('#editor');
   picInfo = querySelector('#picInfo');
-//  iSaver = querySelector('#iSaver')..onChange.listen(saveData);
   btnUpdate = querySelector('#btnUpdate')..onClick.listen(update);
   btnSave = querySelector('#btnSave')..onClick.listen(saveClick);
 
@@ -50,6 +49,7 @@ enter([e]){
 }
 
 init(){
+  quotes = new Map<String, List<PicData>>();
   var rnd = new Math.Random();
   for(var i = 0; i < cube.children.length; i++){
     var face = cube.children[i];
@@ -65,23 +65,29 @@ init(){
     viewBox.append(catBox);
     catBoxes.add(catBox);
     num iconCnt = 1;
-    if (id != 'universe'){
-      for(var side = 0; side < 4; side++){
-        for(var i = 1; i <= (side.isEven ? iconsPerSide : iconsPerSide-2); i++){
-          var iconId = '${id}${iconCnt++}'; // e.g. society1
-          var imgUrl = 'img/i-${iconId}.png';
-          var icon = new DivElement()
-          ..id = 'icon-${iconId}'
-          ..classes.addAll(['icon', 'side$side', id])
-          ..onClick.listen(iconClick)
-          ..style.transform = 'rotate(${rnd.nextInt(20)-10}deg)';
-          catBox.append(icon);
-          ImageElement image = new ImageElement(src: imgUrl);
-          image.onLoad.listen((e){icon.style.backgroundImage = 'url($imgUrl)';}, onError: (e){icon.style.backgroundImage = 'url(img/icon.png)';});
-  //        print('init[$side] $iconId');
+    for(var side = 0; side < 4; side++){
+      for(var i = 1; i <= (side.isEven ? iconsPerSide : iconsPerSide-2); i++){
+        var iconId = 'iconH'; // e.g. society1
+        if ([1,10,19,28].contains(iconCnt)) {
+          iconId = 'icon$iconCnt';
+        } else {
+          iconId = side.isEven ? 'iconH' : 'iconV';
         }
+        var imgUrl = 'img/${iconId}.png';
+        var icon = new DivElement()
+        ..id = 'icon-$iconCnt'
+        ..style.zIndex = '$iconCnt'
+        ..classes.addAll(['icon', 'side$side', id])
+        ..onClick.listen(iconClick)
+        ..style.transform = 'rotate(${rnd.nextInt(20)-10}deg)';
+        catBox.append(icon);
+        ImageElement image = new ImageElement(src: imgUrl);
+        image.onLoad.listen((e){icon.style.backgroundImage = 'url($imgUrl)';}, onError: (e){icon.style.backgroundImage = 'url(img/icon.png)';});
+        print('init[$side] $iconId');
+        iconCnt++;
       }
-    } else {
+    }
+    if (id == 'universe'){
       var btnEnd = new DivElement()
             ..id = 'btnEnd'
             ..text = 'explore the edge of the universe'
@@ -101,7 +107,6 @@ btnEndClick(e){
 }
 
 initQuotes(link){
- quotes = new Map<String, List<PicData>>();
  var dataBody = link.target.import;
   iDs.forEach((id){
     var data = dataBody.body.querySelector('#d-$id');
@@ -115,7 +120,11 @@ initQuotes(link){
           var q = el.querySelector('.quote').innerHtml;
           var a = el.querySelector('.author').innerHtml;
           var p = el.querySelector('.info').innerHtml;
-          entries[i] = new PicData(pdId, q, a, p);
+          var l = el.querySelector('.link').text;
+          var x = num.parse(el.querySelector('.posX').text);
+          var y = num.parse(el.querySelector('.posY').text);
+          var z = num.parse(el.querySelector('.zoom').text);
+          entries[i] = new PicData(picBox, pdId, q, a, p, l, z, x, y);
         }
       }
       quotes[id] = entries;
@@ -127,14 +136,14 @@ iconClick(MouseEvent e){
   if (e.toElement == curIcon) return;
   if (curIcon != null){
     curIcon.classes.remove('selected');
-    showPicBox(false, curIcon.id);
+    showPicBox(false);
     hideQteBox();
   }
   curIcon = e.toElement;
   curIcon.classes.add('selected');
-  curIndex = int.parse(curIcon.id.replaceAll('icon-','').replaceAll('${iDs[curFace]}',''));
+  curIndex = int.parse(curIcon.id.replaceAll('icon-',''));
   print('click on: ${curIcon.id}');
-  showPicBox(true, curIcon.id);
+  showPicBox(true);
 }
 
 resize([e]){
@@ -184,8 +193,7 @@ resize([e]){
       }
     });
   });
-
-  minimize(false);
+  // minimize(false);
 }
 
 showBox(index, show){
@@ -197,21 +205,23 @@ showBox(index, show){
   }
 }
 
-showPicBox(show, [iconId]){
-  var icon = querySelector('#$iconId');
-  if (icon == null && show) return;
-
-  print('[${iconId}]showPicBox($show)');
+showPicBox(show){
+  print('[curIndex]showPicBox($show)');
 
   if (picBox.classes.contains('tvOff')){
     picBox.classes.remove('tvOff');
     querySelector('#btnEnd').style.display = 'block';
   }
-  if (show){
-    picBox.style.backgroundImage = 'url(img/${icon.id.replaceAll('icon-','')}.png)';
+
+  var picData = quotes[iDs[curFace]] != null ? quotes[iDs[curFace]][curIndex] : null;
+  if (show && picData != null) {
+    picData.updateViewer();
     showQteBox();
   } else {
-    picBox.style.backgroundImage = 'url(img/${iDs[curFace]}.png)';
+    picBox
+    ..style.backgroundImage = 'url(img/${iDs[curFace]}.png)'
+    ..style.backgroundPosition = '0px 0px'
+    ..style.backgroundSize = 'cover';
     hideQteBox();
   }
 }
@@ -224,10 +234,12 @@ hideQteBox(){
 }
 
 showQteBox([e]){
+  if (!canEdit) return;
+
   editor
-  ..querySelector('#tQuote').value = ''
-  ..querySelector('#tAuthor').value = ''
-  ..querySelector('#tInfo').value = '';
+  ..querySelector('#eId').text = iDs[curFace]
+  ..querySelector('#eNum').text = curIndex.toString()
+  ..classes.remove('hidden');
 
   if ((qteBox.classes.contains('hidden') || (e is bool && e)) &&
      quotes[iDs[curFace]] != null &&
@@ -242,20 +254,27 @@ showQteBox([e]){
     editor
     ..querySelector('#tQuote').value = picData.quote.replaceAll('<br>', '\n')
     ..querySelector('#tAuthor').value = picData.author.replaceAll('<br>', '\n')
-    ..querySelector('#tInfo').value = picData.info.replaceAll('<br>', '\n');
+    ..querySelector('#tInfo').value = picData.info.replaceAll('<br>', '\n')
+    ..querySelector('#iLink').value = picData.link
+    ..querySelector('#ePicName').href = picData.link
+    ..querySelector('#ePicName').text = picData.link
+    ..querySelector('#iZoom').value = picData.zoom.toStringAsFixed(2)
+    ..querySelector('#iZoom').max = (picData.zoom * 2).toStringAsFixed(2)
+    ..querySelector('#iZoom').step = (picData.zoom / 20).toStringAsFixed(2);
 
     picInfo
     ..innerHtml = picData.info.replaceAll('<br>', '\n')
     ..classes.remove('hidden');
-
-  }
-
-  if (canEdit){
+  } else if ((e is bool && !e)){
     editor
-    ..querySelector('#eId').text = iDs[curFace]
-    ..querySelector('#eNum').text = curIndex.toString()
-    ..querySelector('#ePicName').text = 'img/${iDs[curFace]}${curIndex}.png'
-    ..classes.remove('hidden');
+     ..querySelector('#tQuote').value = ''
+     ..querySelector('#tAuthor').value = ''
+     ..querySelector('#tInfo').value = ''
+     ..querySelector('#iLink').value = ''
+     ..querySelector('#iZoom').min = '0.0'
+     ..querySelector('#iZoom').max = '1.2'
+     ..querySelector('#iZoom').step = '1.2'
+     ..querySelector('#iZoom').value = '1.0';
   }
 }
 
@@ -296,9 +315,12 @@ update([e]){
   var quote = editor.querySelector('#tQuote').value.replaceAll('\n','<br>');
   var author = editor.querySelector('#tAuthor').value.replaceAll('\n','<br>');
   var info = editor.querySelector('#tInfo').value.replaceAll('\n','<br>');
-  var picData = new PicData(id, quote, author, info);
+  var link = editor.querySelector('#iLink').value;
+  var zoom = editor.querySelector('#iZoom').value;
+  var picData = new PicData(picBox, id, quote, author, info, link, num.parse(zoom), 0, 0);
   quotes[iDs[curFace]][curIndex] = picData;
   showQteBox(true);
+  showPicBox(true);
   mustSave = true;
 }
 
@@ -336,10 +358,67 @@ saveClick(e){
 }
 
 class PicData{
-  String id, quote, author, info;
-  PicData(this.id, this.quote, this.author, this.info){}
+  String id, quote, author, info, link='';
+  num posX = 0, posY = 0, zoom = 1.0;
+  num _minZoom = 0.0, _maxZoom = 1.0;
+  int _width = 500, _height=500;
+  ImageElement _img;
+  Element viewer;
+
+  PicData(this.viewer, this.id, this.quote, this.author, this.info, [this.link, this.zoom, this. posX, this.posY]){
+    if (link.isNotEmpty){
+      _img = new ImageElement(src: link);
+      _img.onLoad.listen(_init);
+    }
+  }
+
+  _init(e){
+    _width = _img.naturalWidth;
+    _height = _img.naturalHeight;
+    _img = null;
+
+    var vSize = viewer.clientWidth >= viewer.clientHeight ? viewer.clientHeight : viewer.clientWidth;
+     var iSize = _width > _height ? _height : _width;
+    if (iSize > vSize){
+      _minZoom = _width > _height ? vSize / _height : vSize / _width;
+      _maxZoom = 1.2;
+    } else {
+      _maxZoom = _width > _height ? vSize / _height : vSize / _width;
+      _minZoom = iSize / vSize;
+    }
+    _resize();
+  }
+
+  _resize(){
+    if (viewer != null){
+      if (zoom < _minZoom) zoom = _minZoom;
+      if (zoom > _maxZoom) zoom = _maxZoom;
+      var maxX = (viewer.clientWidth - _width * zoom).round();
+      posX = posX < maxX ? maxX : posX;
+      var maxY = (viewer.clientHeight - _height * zoom).round();
+      posY = posY < maxY ? maxY : posY;
+      posX = posX > 0 ? 0 : posX;
+      posY = posY > 0 ? 0 : posY;
+    }
+  }
+
+  updateViewer(){
+    resize();
+    if(viewer != null){
+      var w = (_width*zoom).round();
+      var h = (_height*zoom).round();
+      viewer
+      ..style.backgroundImage = 'url($link)'
+      ..style.backgroundSize = '${w}px ${h}px}'
+      ..style.backgroundPositionX = '${posX}px)'
+      ..style.backgroundPositionY = '${posY}px)';
+      print('updated-size(z: $zoom) ${viewer.style.backgroundSize}');
+      print('updated-pos: x($posX): ${viewer.style.backgroundPositionX} y($posY): ${viewer.style.backgroundPositionY}');
+    }
+  }
+
   String get asQteHtml => '${quote}<div class="qAuthor"><hr>$author</div>';
-  String get asHtml => '\n        <div id="$id">\n          <div class="quote">$quote</div>\n          <div class="author">$author</div>\n          <div class="info">$info</div>\n        </div>';
+  String get asHtml => '\n        <div id="$id">\n          <div class="quote">$quote</div>\n          <div class="author">$author</div>\n          <div class="info">$info</div>\n          <div class="picInfo"><div class="link">$link</div>\n           <div class="posX">$posX</div>\n           <div class="posY">$posY</div>\n           <div class="zoom">${zoom.toStringAsFixed(3)}</div>\n        </div>\n        </div>';
 }
 
 search(e){
